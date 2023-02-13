c192533b8db70b1d6674251a2ede127b96d5d15a
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-02-14 07:58:03 +0800

    excluded build files

--- dist/data-source.js
+++ dist/data-source.js
@@ -11,14 +11,20 @@ const Product_1 = require("./entity/Product");
 const Sales_1 = require("./entity/Sales");
 const Supplier_1 = require("./entity/Supplier");
 const User_1 = require("./entity/User");
+// const a = new Connection({
+//   url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
+//   type: 'postgres',
+//   port: 5432
+// })
+// a.dropDatabase
 exports.AppDataSource = new typeorm_1.DataSource({
     type: 'postgres',
-    // host: 'dpg-cfl33p9mbjsn9efp9hlg-a',
+    host: 'dpg-cfl33p9mbjsn9efp9hlg-a',
     port: 5432,
-    // username: 'admin',
-    // password: 'KBaNNAj0nORbxVWoa0IWnVgEprfPWdPd',
-    url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
-    // database: 'pos_podi',
+    username: 'admin',
+    password: 'KBaNNAj0nORbxVWoa0IWnVgEprfPWdPd',
+    // url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
+    database: 'pos_podi',
     entities: [
         User_1.User,
         Category_1.Category,
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { DataSource, Connection } from 'typeorm';
 import { AdjustmentStock } from './entity/adjustmentstock';
 import { Category } from './entity/Category';
 import { History } from './entity/History';
@@ -9,14 +9,21 @@ import { Sales } from './entity/Sales';
 import { Supplier } from './entity/Supplier';
 import { User } from './entity/User';
 
+// const a = new Connection({
+//   url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
+//   type: 'postgres',
+//   port: 5432
+// })
+// a.dropDatabase
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  // host: 'dpg-cfl33p9mbjsn9efp9hlg-a',
+  host: 'dpg-cfl33p9mbjsn9efp9hlg-a',
   port: 5432,
-  // username: 'admin',
-  // password: 'KBaNNAj0nORbxVWoa0IWnVgEprfPWdPd',
-  url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
-  // database: 'pos_podi',
+  username: 'admin',
+  password: 'KBaNNAj0nORbxVWoa0IWnVgEprfPWdPd',
+  // url: 'postgres://sdrug556:NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg-cfl3h89mbjsn9efresdg-a/pos_recu',
+  database: 'pos_podi',
   entities: [
     User,
     Category,
