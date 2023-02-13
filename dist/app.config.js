91591e549b051ce2f802b9e5d7830f6c73ef55f1
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-02-13 21:20:51 +0800

    config change

--- dist/app.config.js
+++ dist/app.config.js
@@ -4,18 +4,18 @@ exports.config = void 0;
 exports.config = {
     port: 4201,
     jwtSecretKey: '25a6d0ecae2474b6f7872480d2d1aee6',
-    // database: {
-    //   username: 'postgres', // process.env.PGUSER,
-    //   password: 'A1a$', // process.env.PGPASSWORD,
-    //   database: 'pos', // process.env.PGDATABASE,
-    //   port: 5432,
-    //   host: 'localhost', // process.env.PGHOST,
-    // }
     database: {
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
+        username: 'sdrug556',
+        password: 'NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg',
+        database: 'pos_recu',
         port: 5432,
-        host: process.env.PGHOST,
+        host: 'dpg-cfl3h89mbjsn9efresdg-a', // process.env.PGHOST,
     }
+    // database: {
+    //   username: process.env.PGUSER,
+    //   password: process.env.PGPASSWORD,
+    //   database: process.env.PGDATABASE,
+    //   port: 5432,
+    //   host: process.env.PGHOST,
+    // }
 };
--- src/app.config.ts
+++ src/app.config.ts
@@ -1,19 +1,19 @@
 export const config = {
   port: 4201,
   jwtSecretKey: '25a6d0ecae2474b6f7872480d2d1aee6',
-  // database: {
-  //   username: 'postgres', // process.env.PGUSER,
-  //   password: 'A1a$', // process.env.PGPASSWORD,
-  //   database: 'pos', // process.env.PGDATABASE,
-  //   port: 5432,
-  //   host: 'localhost', // process.env.PGHOST,
-  // }
   database: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    username: 'sdrug556', // process.env.PGUSER,
+    password: 'NW5gyKchOhjejUjpGy1twrWNAJc3xHkA@dpg', // process.env.PGPASSWORD,
+    database: 'pos_recu', // process.env.PGDATABASE,
     port: 5432,
-    host: process.env.PGHOST,
+    host: 'dpg-cfl3h89mbjsn9efresdg-a', // process.env.PGHOST,
   }
+  // database: {
+  //   username: process.env.PGUSER,
+  //   password: process.env.PGPASSWORD,
+  //   database: process.env.PGDATABASE,
+  //   port: 5432,
+  //   host: process.env.PGHOST,
+  // }
 };
 
