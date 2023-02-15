e8e2a822b692bb55f87a44766ab5d5c9eda5fcc0
Mark Jay Delos Santos <marksantos002219@gmail.com>
2023-02-15 20:34:57 +0800

    added cors

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,21 +1,27 @@
 import cors, { CorsOptions } from 'cors';
 
+const origins = [
+  'http://localhost:4200',
+  'http://localhost:4201',
+  'http://localhost:4202',
+  'http://localhost:4204',
+  'http://localhost:4205',
+  'http://localhost:4206',
+  'http://localhost:8080',
+  'http://localhost:8081',
+  'http://localhost:8082',
+  'http://localhost:8083',
+  'http://localhost:8084',
+  'http://localhost:8085',
+];
+
+if (Boolean(process.env.APP_CORS)) {
+  origins.push(...process.env.APP_CORS.split(','));
+}
+
 export class CorsMiddleware {
   public static corsOptions: CorsOptions = {
-    origin: [
-      'http://localhost:4200',
-      'http://localhost:4201',
-      'http://localhost:4202',
-      'http://localhost:4204',
-      'http://localhost:4205',
-      'http://localhost:4206',
-      'http://localhost:8080',
-      'http://localhost:8081',
-      'http://localhost:8082',
-      'http://localhost:8083',
-      'http://localhost:8084',
-      'http://localhost:8085',
-    ],
+    origin: origins,
     optionsSuccessStatus: 204,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
